2aa7c0ab783598958b5c99bdcca6fc1b15c3b5da
Leonid Gaiazov <gaiazov@gmail.com>
2015-10-29 23:27:05 -0400

    Implemented the actual calculator application

--- Program.cs
+++ Program.cs
@@ -5,15 +5,17 @@ namespace ConsoleApplication1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            while (true)
+            {
+                var expressionString = Console.ReadLine();
+                var parser = new ExpressionParser(expressionString);
+                var expression = parser.Parse();
+                var result = expression.Evaluate();
 
-            var expressionString = Console.ReadLine();
-            var parser = new ExpressionParser(expressionString);
-            var expression = parser.Parse();
-            var result = expression.Evaluate();
-
-            Console.WriteLine($"The outcome is {result}");
+                Console.WriteLine($"The outcome is {result}");
+            }
         }
     }
 }
